afd331a0b9f1dde94f150163154c072e29505797
effervescent000 <kislio01@gmail.com>
2022-03-09 14:33:37 -0500

    working on chart implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/main.scss";
 import GeneratePage from "./components/generate-page";
 
 function App() {
--- src/components/chart.js
+++ src/components/chart.js
@@ -7,18 +7,30 @@ const PhonemeChart = ({ phonemes }) => {
             lab: "+",
             cor: 0,
         },
-        alveolar: {
-            cor: 2,
-        },
         dental: {
             cor: 1,
         },
+        alveolar: {
+            cor: 2,
+        },
+        palatal: {
+            cor: 4,
+        },
+        velar: {
+            dor: "+",
+            hgt: "+",
+        },
     };
     const mannerConditions = {
+        nasal: {
+            con: "+",
+            nas: "+",
+        },
         stop: {
             con: "+",
             son: "-",
             cnt: "-",
+            nas: "-",
         },
         fricative: {
             con: "+",
@@ -30,7 +42,7 @@ const PhonemeChart = ({ phonemes }) => {
     const [moa, setMoa] = useState({});
 
     useEffect(() => {
-        const poaCount = { bilabial: 0, dental: 0, alveolar: 0 };
+        const poaCount = { bilabial: 0, dental: 0, alveolar: 0, palatal: 0, velar: 0 };
         const moaCount = { nasal: 0, stop: 0, affricate: 0, fricative: 0 };
         if (phonemes && Object.keys(phonemes).length > 0) {
             for (const [phoneme, values] of Object.entries(phonemes)) {
@@ -41,6 +53,10 @@ const PhonemeChart = ({ phonemes }) => {
                         poaCount.dental++;
                     } else if (values.cor === 2) {
                         poaCount.alveolar++;
+                    } else if (values.cor === 4) {
+                        poaCount.palatal++;
+                    } else if (values.dor === "+" && values.hgt === "+") {
+                        poaCount.velar++;
                     }
 
                     if (values.nas === "+") {
@@ -83,34 +99,36 @@ const PhonemeChart = ({ phonemes }) => {
     };
 
     return (
-        <Table>
-            <thead>
-                <tr>
-                    <th></th>
-                    {Object.keys(placeConditions).map((label) => {
-                        return poa[label] ? <th key={label}>{label}</th> : null;
+        <div className="chart">
+            <Table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        {Object.keys(placeConditions).map((label) => {
+                            return poa[label] ? <th key={label}>{label}</th> : null;
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.entries(mannerConditions).map(([manLabel, manCond]) => {
+                        return moa[manLabel] ? (
+                            <tr key={manLabel} className={manLabel}>
+                                <th>{manLabel}</th>
+                                {Object.entries(placeConditions).map(([plaLabel, plaCond]) => {
+                                    return poa[plaLabel] ? (
+                                        <td key={`${manLabel} ${plaLabel}`}>
+                                            {getPhoneme(plaCond, manCond).map((phoneme) => (
+                                                <span key={phoneme}>{phoneme}</span>
+                                            ))}
+                                        </td>
+                                    ) : null;
+                                })}
+                            </tr>
+                        ) : null;
                     })}
-                </tr>
-            </thead>
-            <tbody>
-                {Object.entries(mannerConditions).map(([manLabel, manCond]) => {
-                    return moa[manLabel] ? (
-                        <tr key={manLabel}>
-                            <th>{manLabel}</th>
-                            {Object.entries(placeConditions).map(([plaLabel, plaCond]) => {
-                                return poa[plaLabel] ? (
-                                    <td key={`${manLabel} ${plaLabel}`}>
-                                        {getPhoneme(plaCond, manCond).map((phoneme) => (
-                                            <span key={phoneme}>{phoneme}</span>
-                                        ))}
-                                    </td>
-                                ) : null;
-                            })}
-                        </tr>
-                    ) : null;
-                })}
-            </tbody>
-        </Table>
+                </tbody>
+            </Table>
+        </div>
     );
 };
 
